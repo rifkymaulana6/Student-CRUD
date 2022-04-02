09160ffa261397bd2bc442e0c7389115833b01b0
rifkymaulana6 <rifkymaulana0612@gmail.com>
2022-04-02 12:27:22 +0700

    Add image screen shots in README.md

--- src/main/java/id/ac/uin/student/entity/Student.java
+++ src/main/java/id/ac/uin/student/entity/Student.java
@@ -3,9 +3,11 @@ package id.ac.uin.student.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -18,18 +20,28 @@ public class Student {
     private Long id;
 
     @NotEmpty(message = "First name is required")
+    @NotNull(message = "First name can not be null!!")
     @Column(nullable = false, name = "first_name")
     private String firstName;
 
     @NotEmpty(message = "Last name is required")
+    @NotNull(message = "Last name can not be null!!")
     @Column(nullable = false, name = "last_name")
     private String lastName;
 
+    @NotNull(message = "Please enter birth date")
+    @Past(message = "Birth date should be less than current date!!")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate birthDate;
+
     @NotEmpty(message = "Email is required")
+    @Email
     @Column(nullable = true, name = "email", unique = true)
     private String email;
 
     @NotEmpty(message = "Phone number is required")
+    @NotNull(message = "Phone number cn not be null!!")
+    @Size(max = 13, min = 10, message = "Phone number should be of 10 digits!!")
     @Column(nullable = true, unique = true)
     private String phoneNumber;
 
